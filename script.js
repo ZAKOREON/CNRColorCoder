6f3c4e6762812d331980d4f1f207ddc84e01b0e5
ZAKOREON <160806085+ZAKOREON@users.noreply.github.com>
2025-01-31 20:32:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const colorCodeData = JSON.parse(localStorage.getItem("CNR_COLOR_CODER_DATA")) ?
 let copiedColorCount = 0;
 
 const e_header = document.getElementById("header");
+const e_editor = document.getElementById("editor");
+const e_editorTextarea = document.getElementById("editorTextarea");
 const e_colorCodeList = document.getElementById("colorCodeList");
 
 e_header.textContent = `0 / ${colorCodeData.length}`;
+
+e_editorTextarea.value = colorCodeData.join("\n");
 
 e_colorCodeList.textContent = "";
 colorCodeData.forEach((color, idx) => {
@@ -18,4 +22,17 @@ const clickColor = () => {
   navigator.clipboard.writeText(color);
   e_header.textContent = `${++copiedColorCount} / ${colorCodeData.length}`;
   target.remove();
+};
+
+const openEditor = () => {
+  e_editor.classList.remove("hide");
+};
+
+let timeout;
+e_editorTextarea.oninput = () => {
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    const data = e_editorTextarea.value.split("\n");
+    localStorage.setItem("CNR_COLOR_CODER_DATA", JSON.stringify(data));
+  }, 1000);
 };
